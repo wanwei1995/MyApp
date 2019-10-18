9f5706f815dea40f118e1a28435dfd1c7c6213d9
万伟 <1254837494@qq.com>
2019-10-18 09:38:29 +0800

    matisse自定义相册引入

--- app/src/main/java/com/example/myapp/ui/main/Fragment/UiFragment.java
+++ app/src/main/java/com/example/myapp/ui/main/Fragment/UiFragment.java
@@ -3,6 +3,7 @@ package com.example.myapp.ui.main.Fragment;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -17,16 +18,20 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.Toast;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
 import butterknife.BindView;
 import com.example.myapp.BaseFragment;
 import com.example.myapp.R;
+import com.example.myapp.glide.GlideLoadEngine;
 import com.example.myapp.myView.MyAdapter;
 import com.example.myapp.ui.GestureActivity;
-import com.example.myapp.ui.main2.activity.CreateZxingActivity;
 import com.example.myapp.ui.main2.dto.MenuDto;
-import com.example.myapp.util.*;
-import com.uuzuche.lib_zxing.activity.CodeUtils;
+import com.example.myapp.util.AlertDialogUtil;
+import com.example.myapp.util.FileUtil;
+import com.example.myapp.util.Global;
+import com.example.myapp.util.StringUtil;
+import com.zhihu.matisse.Matisse;
+import com.zhihu.matisse.MimeType;
+import com.zhihu.matisse.internal.entity.CaptureStrategy;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -78,17 +83,16 @@ public class UiFragment extends BaseFragment {
         uiGridTool.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if(StringUtil.isNotEmpty(menuDTOs.get(i).getLock()) &&  menuDTOs.get(i).getLock().equals("1")){
+                if (StringUtil.isNotEmpty(menuDTOs.get(i).getLock()) && menuDTOs.get(i).getLock().equals("1")) {
                     //上锁
                     Intent intent = new Intent(mContext, GestureActivity.class);
                     Bundle bundle = new Bundle();
-                    bundle.putString("activity",mContext.getPackageName() + ".ui.main.activity." + menuDTOs.get(i).getActivity());
-                    bundle.putString("code",menuDTOs.get(i).getCode());
+                    bundle.putString("activity", mContext.getPackageName() + ".ui.main.activity." + menuDTOs.get(i).getActivity());
+                    bundle.putString("code", menuDTOs.get(i).getCode());
                     intent.putExtras(bundle);
                     startActivity(intent);
                     return;
                 }
-
 
 
                 if (StringUtil.isNotEmpty(menuDTOs.get(i).getCode()) && menuDTOs.get(i).getCode().equals("save_pic")) {
@@ -117,61 +121,77 @@ public class UiFragment extends BaseFragment {
             // We don't have permission so prompt the user
             requestPermissions(Global.PERMISSTIONS_CAMERA, Global.REQUEST_EXTERNAL_STORAGE);
         } else {
-            Intent intentToPickPic = new Intent(Intent.ACTION_PICK, null);
-            // 如果限制上传到服务器的图片类型时可以直接写如："image/jpeg 、 image/png等的类型" 所有类型则写 "image/*"
-            intentToPickPic.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-            startActivityForResult(intentToPickPic, Global.OPEN_ALBUM);
+
+            Matisse.from(UiFragment.this)
+                    .choose(MimeType.ofImage())
+                    .capture(true)
+                    .captureStrategy(new CaptureStrategy(true, "cc.shinichi.bigimageviewpager.fileprovider"))
+                    .countable(true)
+                    .maxSelectable(30)
+                    .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+                    .thumbnailScale(0.85f)
+                    .imageEngine(new GlideLoadEngine())
+                    .theme(com.zhihu.matisse.R.style.Matisse_Zhihu)
+                    .showSingleMediaType(true)
+                    .originalEnable(true)
+                    .forResult(Global.OPEN_ALBUM);
         }
     }
 
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         /**
          * 选择系统图片并解析
          */
         if (requestCode == Global.OPEN_ALBUM) {
             if (data != null) {
-                try {
-                     bitmap = MediaStore.Images.Media.getBitmap(mContext.getContentResolver(), data.getData());
-                }catch (Exception e){
-                    Toast.makeText(mContext, "图片读取失败", Toast.LENGTH_SHORT).show();
-                }
-                //长按存储图片
-                //获取内部存储状态  
-                String state = Environment.getExternalStorageState();
-                //如果状态不是mounted，无法读写  
-                if (!state.equals(Environment.MEDIA_MOUNTED)) {
-                    Toast.makeText(mContext, "系统异常，无法保存", Toast.LENGTH_SHORT).show();
-                    return;
-                }
                 AlertDialogUtil.YesOrNo("是否保存?", mContext, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        FileOutputStream out = null;
-                        try {
-                            File file = FileUtil.createPhotoPath(FileUtil.PRIVATE1, "",true);
-                            out = new FileOutputStream(file);
-                            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-
-                            Toast.makeText(mContext, "已保存到手机，请到相册查看", Toast.LENGTH_SHORT).show();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        } finally {
-                            try {
-                                out.flush();
-                                out.close();
-                                //删除图片
-                                FileUtil.deletePicture(data.getData(),mContext);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
+                        //长按存储图片
+                        //获取内部存储状态  
+                        String state = Environment.getExternalStorageState();
+                        //如果状态不是mounted，无法读写  
+                        if (!state.equals(Environment.MEDIA_MOUNTED)) {
+                            Toast.makeText(mContext, "系统异常，无法保存", Toast.LENGTH_SHORT).show();
+                            return;
                         }
 
-
+                        for (Uri uri : Matisse.obtainResult(data)) {
+                            SavePic(uri);
+                        }
+                        Toast.makeText(mContext, "已保存到手机，请到相册查看", Toast.LENGTH_SHORT).show();
                     }
                 });
+            }
         }
+    }
+
+    private void SavePic(Uri uri) {
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(mContext.getContentResolver(), uri);
+        } catch (Exception e) {
+            Toast.makeText(mContext, "图片读取失败", Toast.LENGTH_SHORT).show();
+            return;
         }
+        FileOutputStream out = null;
+        try {
+            File file = FileUtil.createPhotoPath(FileUtil.PRIVATE1, "", true);
+            out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.flush();
+                out.close();
+                //删除图片
+                FileUtil.deletePicture(uri, mContext);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
